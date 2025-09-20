064d6e1d78cb62caed3a7fbd1363ed7cae7439c5
danmesfin <danielmsfn@gmail.com>
2025-09-20 21:46:43 +0300

    fix: reduce video processor lambda memory from 6GB to 3GB to optimize costs

--- video-processing-api/main.tf
+++ video-processing-api/main.tf
@@ -296,7 +296,7 @@ resource "aws_lambda_function" "processor" {
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
   timeout = 900
-  memory_size = 6144
+  memory_size = 3008
 
   layers = length(local.chosen_ffmpeg_layer_arn) > 0 ? [local.chosen_ffmpeg_layer_arn] : []
 
